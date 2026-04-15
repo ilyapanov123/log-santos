5617ee1468ec9fc498243da349d8f9f99da91c0d
Polina <pollibechova@gmail.com>
2026-04-15 20:32:05 +0300

    returned

--- service/src/main/java/com/bigsmo/service/config/KafkaConfig.java
+++ service/src/main/java/com/bigsmo/service/config/KafkaConfig.java
@@ -8,6 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +24,15 @@ public class KafkaConfig {
 
     @Value("${spring.kafka.consumer.group-id}")
     private String groupId;
+
+  @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule()); 
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); 
+        return mapper;
+    }
+
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
